4a3d73b29b6707a772e04bd0095d755680837400
diego <memories_212@hotmail.com>
2019-05-13 12:09:16 -0500

    messge control

--- app/src/main/java/com/americamovil/jenkins/MainActivity.java
+++ app/src/main/java/com/americamovil/jenkins/MainActivity.java
@@ -2,12 +2,15 @@ package com.americamovil.jenkins;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "consola";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate: prueba");
     }
 }
